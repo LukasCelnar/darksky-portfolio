63c9e7c49815763680538458cf653dc1e14e059f
Drizz159 <celnar.lukas@gmail.com>
2021-03-25 19:46:35 +0100

    Filling LandingPage and Navbar components

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -5,19 +5,22 @@ import Navbar from '../Navbar/Navbar';
 const LandingPage = () => {
 
     return (
-        <div className='landingpage'>
+        <div className='landingpage' style={{backgroundImage: 'url(/images/landing-page/bg.png)'}}>
+            <div className='landingpage__bg-overlay'></div>
             <Navbar />
 
            <div className='landingpage__main'>
                 <div className='landingpage__main-header'>
-
+                    Hey, I’m Lukas
                 </div>
+                <div className='landingpage__main-header-underline'></div>
                 <div className='landingpage__main-text'>
-
+                    Im passionate about coding and building software that can do amazing things.
+                    My life goal is to be part of something big.
                 </div>
            </div>
 
-            <img className='landingpage__scroll' src='/images/skills/scroll.png' alt='scroll' />
+            <img className='landingpage__scroll' src='/images/landing-page/scroll.png' alt='scroll' />
         </div>
     );
 };
