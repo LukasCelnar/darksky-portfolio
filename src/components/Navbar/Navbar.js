2fc2126dbb64f51dbd8b6af1573510622724fad7
Drizz159 <celnar.lukas@gmail.com>
2021-03-27 10:58:35 +0100

    Adding Navbar responsiveness

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
 
 const Navbar = () => {
+    const [toggleBtn, setToggleBtn] = useState(false);
+
+    const onHamburgerMenuClick = () => {
+        setToggleBtn(!toggleBtn);
+    };
+
     return (
         <div className='navbar'>
-           <img className='navbar__logo' src='/images/navbar/lc-logo.png' />
-           <div className='navbar__items'>
+            <img className='navbar__logo' src='/images/navbar/lc-logo.png' />
+            <div className='navbar__items'>
                 <Link to='/' className='navbar__item'>Home</Link>
                 <Link to='/' className='navbar__item'>Work</Link>
                 <Link to='/' className='navbar__item'>About</Link>
                 <Link to='/' className='navbar__item'>Contact</Link>
-           </div>
+            </div>
+            <div className='navbar__hamburger' onClick={() => onHamburgerMenuClick()}>
+                <div className='navbar__hamburger-bar'></div>
+                <div className='navbar__hamburger-bar'></div>
+                <div className='navbar__hamburger-bar'></div>
+            </div>
+            <div className={`navbar__hamburger-bg ${toggleBtn ? 'navbar__hamburger-bg-show' : 'navbar__hamburger-bg-hide'}`}></div>
+            <div className={`navbar__hamburger-items navbar__hamburger-items-show ${toggleBtn ? 'navbar__hamburger-items-show' : 'navbar__hamburger-items-hide'}`}>
+                <div className='navbar__hamburger-item'>Home</div>
+                <div className='navbar__hamburger-item'>Work</div>
+                <div className='navbar__hamburger-item'>About</div>
+                <div className='navbar__hamburger-item'>Contact</div>
+            </div>
         </div>
     );
 };
