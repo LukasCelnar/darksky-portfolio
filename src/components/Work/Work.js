5a91d708902d445a276605f6999af1127d694d32
Drizz159 <celnar.lukas@gmail.com>
2022-12-06 11:31:59 +0100

    fixing live and github buttons

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -24,8 +24,8 @@ const Work = () => {
                         <div className='work__image-hover-header'>{work.heading}</div>
                         <div className='work__image-hover-content'>{work.content}</div>
                         <div className='work__image-hover-links'>
-                            {work.github ? <a href="https://google.com"><img className='work__image-hover-link' src='/images/work/github-white.png' alt='github-logo' /></a> : null}
-                            <a href="https://google.com" style={work.live ? {} : {display: 'none'}}><img className='work__image-hover-link' src='/images/work/live-white.png' alt='live-logo'/></a>
+                            {work.github ? <a href={work.github}><img className='work__image-hover-link' src='/images/work/github-white.png' alt='github-logo' /></a> : null}
+                            <a href={work.live} style={work.live ? {} : {display: 'none'}}><img className='work__image-hover-link' src='/images/work/live-white.png' alt='live-logo'/></a>
                         </div>
                     </div>
                 </div>
